017aa360c67aab9c8e2d92b755a800d45831d531
Григорий Кривобоков <danilazaiac@gmail.com>
2024-02-25 18:13:23 +0300

    Version 1.1.2

--- RegisterBotanicGarden/AdminPanel.xaml.cs
+++ RegisterBotanicGarden/AdminPanel.xaml.cs
@@ -19,9 +19,42 @@ namespace RegisterBotanicGarden
     /// </summary>
     public partial class AdminPanel : Window
     {
+        private Dictionary<string, UIElement> pairs = new Dictionary<string, UIElement>();
+        string lastname = "Null";
+
         public AdminPanel()
         {
             InitializeComponent();
+
+            pairs.Add("Null", Info1);
+            pairs.Add("PersonPage1", Persons1);
+            pairs.Add("GardenPage1", Garden1);
+            pairs.Add("InventoryPage1", Inventory1);
+            pairs.Add("TasksPage1", Tasks1);
+
+            foreach (var i in pairs)
+                if(i.Key != "Null")
+                    Content1.Children.Remove(i.Value);
+        }
+
+        private void Pages_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (lastname == button.Name)
+            {
+                Content1.Children.Remove(pairs[lastname]);
+                lastname = "Null";
+                Content1.Children.Add(pairs[lastname]);
+                return;
+            }
+
+            if (lastname != "")
+                Content1.Children.Remove(pairs[lastname]);
+
+            lastname = button.Name;
+
+            Content1.Children.Add(pairs[lastname]);
         }
     }
 }
